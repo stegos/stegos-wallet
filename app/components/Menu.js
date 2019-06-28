c0cb087ae8d8515a7ca0339794d31b48aeebb170
anton <menshickov@softmotions.com>
2019-06-28 17:46:58 +0700

    header & bugs and terms fix

--- app/components/Menu.js
+++ app/components/Menu.js
@@ -5,16 +5,27 @@ import routes from '../constants/routes';
 
 import style from './Menu.css';
 
-class Menu extends Component {
-  render = () => (
-    <div className={style.Menu}>
-      <Link to={routes.PORTFOLIO}>Portfolio</Link>
-      <Link to={routes.ACCOUNTS}>Accounts</Link>
-      <Link to={routes.SEND}>Send</Link>
-      <Link to={routes.RECEIVE}>Receive</Link>
-      <Link to={routes.NODE}>Node</Link>
-    </div>
-  );
+type Props = {
+  className?: string
+};
+
+class Menu extends Component<Props> {
+  static defaultProps = {
+    className: ''
+  };
+
+  render() {
+    const { className } = this.props;
+    return (
+      <div className={`${style.Menu} ${className}`}>
+        <Link to={routes.PORTFOLIO}>Portfolio</Link>
+        <Link to={routes.ACCOUNTS}>Accounts</Link>
+        <Link to={routes.SEND}>Send</Link>
+        <Link to={routes.RECEIVE}>Receive</Link>
+        <Link to={routes.NODE}>Node</Link>
+      </div>
+    );
+  }
 }
 
 export default connect(
